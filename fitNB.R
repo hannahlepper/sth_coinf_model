e1e282a29180bcd9d7a0855f3d5aae931a2be625
hannahlepper <hannah.c.lepper@gmail.com>
2018-08-27 20:11:26 +0100

    suppress warnings

--- fitNB.R
+++ fitNB.R
@@ -1,13 +1,19 @@
 #Fit NB function
 fit_nb <- function(xs) {
-  if(sum(xs > 0)) {
-    fit <- try(MASS::fitdistr(xs, "negative binomial"))
-    if(class(fit) == "fitdistr") {
-      fit$estimate
+  suppressWarnings(
+    if(sum(xs > 0)) {
+      fit <- try(MASS::fitdistr(xs, "negative binomial"))
+      if(class(fit) == "fitdistr") {
+        fit$estimate
+      } else {
+        c(0, 0)
+      }
     } else {
       c(0, 0)
     }
-  } else {
-    c(0, 0)
-  }
+  )
 }
+
+# Test
+# dist <- rnbinom(100, size = 0.1, mu = 2)
+# fit <- fit_nb(dist)
