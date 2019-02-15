1e5848a5fff5004ff930c010362fd6c803286e11
hannahlepper <hannah.c.lepper@gmail.com>
2019-02-15 16:43:34 +0000

    scale before distance calculation

--- ABC.jl
+++ ABC.jl
@@ -6,10 +6,10 @@ mean_obs = [6.9, 249.6, 38.8]
 k_obs = [0.08, 0.1, 0.21]
 
 # Prior particles - using uniform distribution
-Imme_activation = rand(Uniform(0, 1),33)
-Immf_activation = rand(Uniform(0, 1),33)
-est_modulation = rand(Uniform(0, 1),33)
-fec_modulation = rand(Uniform(0, 1),33)
+Imme_activation = rand(Uniform(0, 1),333)
+Immf_activation = rand(Uniform(0, 1),333)
+est_modulation = rand(Uniform(0, 1),333)
+fec_modulation = rand(Uniform(0, 1),333)
 
 
 #Map model over priors.
@@ -29,9 +29,9 @@ function run_particle(sp, particle)
 end
 
 #Generate simulated dataset
-sim_ds = zeros(Float64, 2000, 3, 33) #empty to contain the simulated values
+sim_ds = zeros(Float64, 2000, 3, 333) #empty to contain the simulated values
 
-@time for s in 1:33
+@time for s in 1:333
   sim_ds[:,:,s] = run_particle(1, [Imme_activation[s],
                                    Immf_activation[s],
                                    est_modulation[s],
@@ -54,29 +54,43 @@ using RCall
 R"source('fitNB.R')"
 @rget fit_nb
 
-fitted_vals = zeros(Float64, 2, 3, 33)
-for s in 1:33
+fitted_vals = zeros(Float64, 2, 3, 333)
+for s in 1:333
   for sp in 1:3
     fitted_vals[:,sp,s] = fit_nb(sim_ds_sampled[:,sp,s])
   end
 end
 
-fitted_vals[1,1,:]
-# check for correlation
-plot(fitted_vals[1,1,:], fitted_vals[1,2,:], st = :scatter)
+# Scale
+
+function root_mean_square(xs)
+  n = length(xs)
+  sqrt(sum(xs.^2)/(n-1))
+end
+
+scale_pars = [root_mean_square(fitted_vals[v,sp,:]) for sp in 1:3, v in 1:2]
+fitted_vals_scaled = zeros(Float64, 2, 3, 333)
+for sp in 1:3
+  for v in 1:2
+    fitted_vals_scaled[v,sp,:] = fitted_vals[v,sp,:]./scale_pars[sp,v]
+  end
+end
+
+k_obs_scaled = k_obs./scale_pars[:,1]
+mean_obs_scaled = mean_obs./scale_pars[:,2]
 
 #Calculate basic distances between sim vals and target vals
-dists = zeros(Float64, 33, 2)
-dists[:,1] = fitted_vals[1,1,:] .- k_obs[1]
-dists[:,2] = fitted_vals[2,1,:] .- mean_obs[1]
+dists = zeros(Float64, 333, 2)
+dists[:,1] = fitted_vals_scaled[1,1,:] .- k_obs_scaled[1]
+dists[:,2] = fitted_vals_scaled[2,1,:] .- mean_obs_scaled[1]
 
 #Calculate standardised euclidean distance
 function euc(dists)
     sqrt(sum(dists.^2))
 end
 
-euc_dists = zeros(Float64, 33, 1)
-for s in 1:33
+euc_dists = zeros(Float64, 333, 1)
+for s in 1:333
   euc_dists[s] = euc(dists[s,:])
 end
 
@@ -86,7 +100,7 @@ histogram(euc_dists[:,1])
 #Get posteriors
 #Accept small percent
 
-accept = findall(i -> i < 8, euc_dists) #change 6 after looking at histogram
+accept = findall(i -> i < 1.65, euc_dists) #change 6 after looking at histogram
 
 #Print un-squeezed posteriors
 using StatsPlots
@@ -94,6 +108,11 @@ density(Imme_activation[accept])
 density(Immf_activation[accept])
 density(fec_modulation[accept])
 density(est_modulation[accept])
+
+histogram(Imme_activation[accept])
+histogram(Immf_activation[accept])
+histogram(fec_modulation[accept])
+histogram(est_modulation[accept])
 
 
 #Squeeze posteriors
